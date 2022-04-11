3baa6ad06f8eb6c2d1afcee083874dc118fc0130
Timo Schuermann <timo.schuermann91@gmx.de>
2022-04-11 09:48:43 +0200

    fix

--- R/CountPerIP.R
+++ R/CountPerIP.R
@@ -10,7 +10,7 @@
 #'
 #' @examples
 CountPerIP <- function(exb, Variable="V", Variante="Variante", format="dataframe"){
-  CountPerIP <- exb %>% dplyr::rename(Variable={{Variable}}) %>% dplyr::rename(Variante={{Variante}}) %>% dplyr::filter(!is.na(Variable)) %>%   dpylr::group_by(IpNumber,Variable, Variante) %>% dplyr::count() %>% dplyr::mutate(Variante= str_c(Variable,"_",Variante )) %>% dplyr::ungroup() %>% dplyr::select(-Variable) %>%  tidyr::pivot_wider(names_from = Variante, values_from = n) %>% dplyr::mutate(dplyr::across(everything(), .fns = ~tidyr::replace_na(.,0))) #%>% dplyr::select(-IpNumber)
+  CountPerIP <- exb %>% dplyr::rename(Variable={{Variable}}) %>% dplyr::rename(Variante={{Variante}}) %>% dplyr::filter(!is.na(Variable)) %>%   dplyr::group_by(IpNumber,Variable, Variante) %>% dplyr::count() %>% dplyr::mutate(Variante= str_c(Variable,"_",Variante )) %>% dplyr::ungroup() %>% dplyr::select(-Variable) %>%  tidyr::pivot_wider(names_from = Variante, values_from = n) %>% dplyr::mutate(dplyr::across(everything(), .fns = ~tidyr::replace_na(.,0))) #%>% dplyr::select(-IpNumber)
   if(format== "dataframe"){
   return(CountPerIP)
   }else if(format=="matrix"){
