b2b7ee954d3be5043e4ba8a4c3d795685bd7a673
Timo Schuermann <timo.schuermann91@gmx.de>
2022-04-11 09:24:34 +0200

    delete

--- R/recodeVars.R
+++
@@ -1,4 +0,0 @@
-recodeVars <- function(Corpus, VarCol="V",VariantCol="Variante", Variable, oldVariants, newVariants){
-  oldVariants <- stringr::str_flatten(oldVariants, collapse = "|")
-  Corpus %>% mutate(VariantNew= dplyr::case_when({{VarCol}}=={{Variable}} & stringr::str_detect({{VariantCol}}, oldVariants)~ newVariants, TRUE ~ pull(Corpus,{{VariantCol}})))
-}
